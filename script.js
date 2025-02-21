d8a46392d5bfacf09dbdffbac1f411ba7f6e8c23
JoshK7 <joshk7683@gmail.com>
2025-02-20 21:22:23 -0600

    initial setup for validation

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const form = document.querySelector("form");
 const queryItems = form.querySelectorAll(".query__list--item");
 
+const validations = {
+    "given-name": (value) => {
+
+    },
+    "family-name": (value) => {
+
+    },
+    "email": (value) => {
+
+    },
+    "query": (value) => {
+
+    },
+    "message": (value) => {
+
+    },
+    "consent": (value) => {
+        
+    },
+}
+
+const validate = (formData) => {
+    for (const key in validations) {
+        if (!Object.hasOwn(formData, key)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 const handleQueryClick = (e) => {
     const button = e.target.querySelector("input[type=radio]");
     if (button) {
@@ -8,6 +39,15 @@ const handleQueryClick = (e) => {
     }
 }
 
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(form));
+    console.log(data, validate(data));
+}
+
 queryItems.forEach((item) => {
     item.addEventListener("click", handleQueryClick);
 });
+
+form.addEventListener("submit", handleFormSubmit);
