1dd46b2813e76207ce72095708f9be9f97052222
JoshK7 <joshk7683@gmail.com>
2025-02-21 21:41:57 -0600

    further work on validation

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 const form = document.querySelector("form");
 const queryItems = form.querySelectorAll(".query__list--item");
 
+const emailRegExp =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 const validations = {
     "given-name": (value) => {
-
+        return !!value.trim();
     },
     "family-name": (value) => {
-
+        return !!value.trim();
     },
     "email": (value) => {
-
+        return value.length !== 0 && emailRegExp.test(value);
     },
     "query": (value) => {
-
+        return !!value.trim();
     },
     "message": (value) => {
-
+        return !!value.trim();
     },
     "consent": (value) => {
-        
+        return !!value.trim();
     },
 }
 
 const validate = (formData) => {
     for (const key in validations) {
         if (!Object.hasOwn(formData, key)) {
+            return false;
+        }
+
+        if (!validations[key](formData[key])) {
+            console.log(key);
             return false;
         }
     }
