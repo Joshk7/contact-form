b53a47683916f528dfb1740d202ecdd9fcb34c19
JoshK7 <joshk7683@gmail.com>
2025-02-22 18:47:44 -0600

    added error elements in js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const form = document.querySelector("form");
+const errorGiven = document.getElementById("error_given");
+const errorFamily = document.getElementById("error_family");
+const errorEmail = document.getElementById("error_email");
+const errorQuery = document.getElementById("error_query");
+const errorMessage = document.getElementById("error_message");
+const errorConsent = document.getElementById("error_consent");
+
 const queryItems = form.querySelectorAll(".query__list--item");
 
 const emailRegExp =
@@ -11,19 +18,28 @@ const validations = {
     "family-name": (value) => {
         return !!value.trim();
     },
-    "email": (value) => {
+    email: (value) => {
         return value.length !== 0 && emailRegExp.test(value);
     },
-    "query": (value) => {
+    query: (value) => {
         return !!value.trim();
     },
-    "message": (value) => {
+    message: (value) => {
         return !!value.trim();
     },
-    "consent": (value) => {
+    consent: (value) => {
         return !!value.trim();
     },
-}
+};
+
+const errors = {
+    "given-name": errorGiven,
+    "family-name": errorFamily,
+    email: errorEmail,
+    query: errorQuery,
+    message: errorMessage,
+    consent: errorConsent,
+};
 
 const validate = (formData) => {
     for (const key in validations) {
@@ -32,27 +48,26 @@ const validate = (formData) => {
         }
 
         if (!validations[key](formData[key])) {
-            console.log(key);
             return false;
         }
     }
 
     return true;
-}
+};
 
 const handleQueryClick = (e) => {
     const button = e.target.querySelector("input[type=radio]");
     if (button) {
         button.click();
     }
-}
+};
 
 const handleFormSubmit = (e) => {
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(form));
-    console.log(data, validate(data));
-}
+    validate(data);
+};
 
 queryItems.forEach((item) => {
     item.addEventListener("click", handleQueryClick);
